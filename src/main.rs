85983bd4d6c679b867444cb083039d7111f0c3ae
Roel de Jong <roel@getrodeo.io>
2021-04-20 21:01:32 +0200

    add catsay setup

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+extern crate structopt;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Options {
+    #[structopt(default_value = "Meow!")]
+    /// What does the cat say?
+    message: String,
+}
+
+fn main() {
+    let options = Options::from_args();
+    let message = options.message;
+    println!("{}", message);
+    println!(" \\");
+    println!("  \\");
+    println!("    /\\_/\\");
+    println!("   ( o o )");
+    println!("   =( I )=");
+}
