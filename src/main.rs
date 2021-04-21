86f556c497807ccee56d5a3934717acb1a2b53aa
Roel de Jong <Roel4811@hotmail.com>
2021-04-21 08:38:16 +0200

    add file input and error handling

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 extern crate structopt;
 use structopt::StructOpt;
 
+extern crate colored;
+use colored::*;
+
+use failure::ResultExt;
+use exitfailure::ExitFailure;
+
 #[derive(StructOpt)]
 #[structopt(name = "Catsay", about = "cli app with a talking cat")]
 struct Options {
@@ -11,9 +17,13 @@ struct Options {
     #[structopt(short = "d", long = "dead")]
     /// Make the cat appear dead
     dead: bool,
+
+    #[structopt(short = "f", long = "file", parse(from_os_str))]
+    /// Load the cat picture from the specified file
+    catfile: Option<std::path::PathBuf>,
 }
 
-fn main() {
+fn main() -> Result<(), ExitFailure> {
     let options = Options::from_args();
     let message = options.message;
     if message.to_lowercase() == "woof" {
@@ -22,10 +32,22 @@ fn main() {
 
     let eye = if options.dead { "x" } else { "o" };
 
-    println!("{}", message);
-    println!(" \\");
-    println!("  \\");
-    println!("    /\\_/\\");
-    println!("   ( {eye} {eye} )", eye = eye);
-    println!("   =( I )=");
+    match &options.catfile {
+        Some(path) => {
+            let cat_template = std::fs::read_to_string(path)
+                .with_context(|_| format!("could not read file {:?}",
+                    path))?;
+            let cat_picture = cat_template.replace("{eye}", eye);
+            println!("{}", &cat_picture);
+        },
+        None => {
+            println!("{}", message.bright_yellow().underline().on_purple());
+            println!(" \\");
+            println!("  \\");
+            println!("    /\\_/\\");
+            println!("   ( {eye} {eye} )", eye = eye.red().bold());
+            println!("   =( I )=");
+        },
+    }
+    Ok(())
 }
